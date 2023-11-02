a805f16c9a53c5bca6e88868caeb66e66063d79e
Henrique Almeida <marceloh500@gmail.com>
2023-11-02 00:50:57 -0300

    Atualização de link de projetos

--- src/pages/Projetos.js
+++ src/pages/Projetos.js
@@ -3,23 +3,20 @@ import BlocoProjeto from "../components/BlocoProjeto";
 import Urna from "./../assets/img/urna.png";
 import Clima from "./../assets/img/clima.png";
 import Calculadora from "./../assets/img/calculadora.png";
-import Todo from "./../assets/img/todolist.png";
+import Todo from "./../assets/img/img_todo.png";
 import { FaHtml5, FaReact } from "react-icons/fa";
 import { IoLogoJavascript } from "react-icons/io5";
 import { SiStyledcomponents } from "react-icons/si";
 
 function Projetos() {
   function abrirCalculadora() {
-    window.open("https://calculadora-mobile-five.vercel.app/", "_blank");
+    window.open("https://calc7uladora.vercel.app/", "_blank");
   }
   function abrirUrna() {
     window.open("https://urna-eletronica-pi-snowy.vercel.app/", "_blank");
   }
   function abrirClima() {
-    window.open(
-      "https://sistema-clima-do-tempo-crq1x5llk-henrique-almeidas-projects.vercel.app/",
-      "_blanck"
-    );
+    window.open("https://cli3ma.vercel.app/", "_blanck");
   }
   function abrirTodo() {
     window.open("https://to-do-list-gold-seven.vercel.app/", "_blank");
@@ -32,7 +29,7 @@ function Projetos() {
         <BlocoProjeto
           onClick={abrirUrna}
           imagem={Urna}
-          nome="Urna Eletrônica"
+          nome="Urna (Em Construção)"
           tecnologia1={<FaHtml5 />}
           tecnologia2={<SiStyledcomponents />}
           tecnologia3={<IoLogoJavascript />}
